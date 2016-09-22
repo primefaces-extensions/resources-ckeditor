abc52da8092cbb476ef94fe0ca0d247e60a30fbf
Emil Lefkof <elefkof@ksmpartners.com>
2016-09-22 15:25:18 -0400

    Updated Git URL's based on Sonatype guide. Removed redundant elements. Changed commons lang to 3.1 which is the version inherited from the parent project and used in PF Extensions Core.

--- src/main/java/org/primefaces/extensions/ckeditor/ResourceModifier.java
+++ src/main/java/org/primefaces/extensions/ckeditor/ResourceModifier.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Steps to update:
