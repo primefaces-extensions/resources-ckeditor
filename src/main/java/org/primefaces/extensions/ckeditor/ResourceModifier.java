6ef828a7bcecc188bd8b8c4b4af9b925a643a5a2
melloware <mellowaredev@gmail.com>
2017-11-28 09:28:23 -0500

    - Fixed license - Cleaned up formatting - Only read ckeditor.js once and write it out once.

--- src/main/java/org/primefaces/extensions/ckeditor/ResourceModifier.java
+++ src/main/java/org/primefaces/extensions/ckeditor/ResourceModifier.java
@@ -1,7 +1,5 @@
-package org.primefaces.extensions.ckeditor;
-
-/*
- * Copyright 2011-2015 PrimeFaces Extensions
+/**
+ * Copyright 2011-2017 PrimeFaces Extensions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +12,8 @@ package org.primefaces.extensions.ckeditor;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * $Id$
  */
+package org.primefaces.extensions.ckeditor;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,131 +25,131 @@ import org.apache.commons.lang3.StringUtils;
 
 /**
  * Steps to update:
+ * 
+ * <pre>
  * - Delete all files in the src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/ directory
  * - extract new ckeditor files
  * - remove samples dir
  * - Add an empty skin.js file to the skin directory which does not contain a default skin.js
+ * </pre>
  */
 public class ResourceModifier {
 
-	private static final String PROJECT_DIRECTORY = System.getProperty("user.dir");
+    private static final String PROJECT_DIRECTORY = System.getProperty("user.dir");
 
-	public static void main(String[] args) throws IOException {
+    public static void main(final String[] args) throws IOException {
 
-        String resourcesDirectory = PROJECT_DIRECTORY + "/src/main/resources/META-INF/resources/primefaces-extensions/";
+        final String resourcesDirectory = PROJECT_DIRECTORY + "/src/main/resources/META-INF/resources/primefaces-extensions/";
 
         System.err.println("######## Modify skin styles....");
 
-        File skinsPath = new File(PROJECT_DIRECTORY + "/src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/skins/");
-        
-		for (String skin : skinsPath.list()) {
-			System.err.println("#### Modify skin '" + skin + "'");
+        final File skinsPath = new File(PROJECT_DIRECTORY + "/src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/skins/");
 
-			File skinDirectory = new File(resourcesDirectory + "ckeditor/skins/" + skin);
-            String relativeSkinPath =  StringUtils.replace(
+        for (final String skin : skinsPath.list()) {
+            System.err.println("#### Modify skin '" + skin + "'");
+
+            final File skinDirectory = new File(resourcesDirectory + "ckeditor/skins/" + skin);
+            final String relativeSkinPath = StringUtils.replace(
                         StringUtils.replace(skinDirectory.getPath(), resourcesDirectory, ""),
-                        "\\", 
+                        "\\",
                         "/");
 
-			for (File resourceToModify : skinDirectory.listFiles()) {
+            for (final File resourceToModify : skinDirectory.listFiles()) {
 
                 // modify css only
-				if (!resourceToModify.getName().endsWith(".css")) {
-					continue;
-				}
+                if (!resourceToModify.getName().endsWith(".css")) {
+                    continue;
+                }
 
-				System.err.println("## Modify file '" + resourceToModify.getName() + "'");
+                System.err.println("## Modify file '" + resourceToModify.getName() + "'");
 
-				String fileContent = FileUtils.readFileToString(resourceToModify);
-                List<File> allSkinResources = getResourcesList(skinDirectory);
+                String fileContent = FileUtils.readFileToString(resourceToModify);
+                final List<File> allSkinResources = getResourcesList(skinDirectory);
 
                 // loop all possible image references
-				for (File resource : allSkinResources) {
-                    
+                for (final File resource : allSkinResources) {
+
                     // we just need to check included images
                     if (resource.getName().endsWith(".css")) {
                         continue;
                     }
 
-					String relativeResourcePath = StringUtils.replace(
-                            StringUtils.replace(resource.getPath(), resourcesDirectory, ""),
-                            "\\", 
-                            "/");
+                    final String relativeResourcePath = StringUtils.replace(
+                                StringUtils.replace(resource.getPath(), resourcesDirectory, ""),
+                                "\\",
+                                "/");
                     String resourceName = StringUtils.replace(
-                            resource.getAbsolutePath().replace(skinDirectory.getAbsolutePath(), ""),
-                            "\\",
-                            "/");
+                                resource.getAbsolutePath().replace(skinDirectory.getAbsolutePath(), ""),
+                                "\\",
+                                "/");
 
                     if (resourceName.startsWith("/")) {
                         resourceName = resourceName.substring(1);
                     }
 
-					fileContent = fileContent.replaceAll("url\\(" + resourceName + "\\)",
-							"url\\(\"#{resource['primefaces-extensions:" + relativeResourcePath + "']}\"\\)");
-				}
+                    fileContent = fileContent.replaceAll("url\\(" + resourceName + "\\)",
+                                "url\\(\"#{resource['primefaces-extensions:" + relativeResourcePath + "']}\"\\)");
+                }
 
                 // icons.png
                 fileContent = fileContent.replaceAll("url\\(icons.png\\?t=GAGE",
-                        "url\\(\"#{resource['primefaces-extensions:" + relativeSkinPath + "/icons.png']}&t=GAGE\"");
-                
+                            "url\\(\"#{resource['primefaces-extensions:" + relativeSkinPath + "/icons.png']}&t=GAGE\"");
+
                 // icons_hidpi.png
                 fileContent = fileContent.replaceAll("url\\(icons_hidpi.png\\?t=GAGE",
-                        "url\\(\"#{resource['primefaces-extensions:" + relativeSkinPath + "/icons_hidpi.png']}&t=GAGE\"");
-                
-                
-				FileUtils.writeStringToFile(resourceToModify, fileContent);
-			}
+                            "url\\(\"#{resource['primefaces-extensions:" + relativeSkinPath + "/icons_hidpi.png']}&t=GAGE\"");
 
-			String fileContent = "";
-			File file = null;
+                FileUtils.writeStringToFile(resourceToModify, fileContent);
+            }
 
-			// modify smileys plugin to load the smileys via CKEditor.getUrl
-			file = new File(PROJECT_DIRECTORY
-					+ "/src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/plugins/smiley/dialogs/smiley.js");
-			fileContent = FileUtils.readFileToString(file).replaceAll(
-					"CKEDITOR.tools.htmlEncode\\(e\\.smiley_path\\+h\\[a\\]\\)",
-					"CKEDITOR.tools.htmlEncode\\(CKEDITOR.getUrl\\(e\\.smiley_path\\+h\\[a\\]\\)\\)");
-			FileUtils.writeStringToFile(file, fileContent);
-		}
-		
-		String fileContent = "";
-		File file = null;
-		
-		// modify copyFormatting plugin to load copyformatting.css via CKEditor.getUrl
-		file = new File(resourcesDirectory
-				+ "ckeditor/ckeditor.js");
-		fileContent = FileUtils.readFileToString(file).replaceAll(
-				"this.path\\+\"styles/copyformatting.css\"", 
-				"CKEDITOR.getUrl(this.path\\+\"styles/copyformatting.css\")");
-		FileUtils.writeStringToFile(file, fileContent);
-		
-		// modify scayt plugin to load scayt.css via CKEditor.getUrl
-		file = new File(resourcesDirectory
-                        + "ckeditor/ckeditor.js");
-		fileContent = FileUtils.readFileToString(file).replaceAll(
-		                "this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/scayt.css\"", 
-		                "CKEDITOR.getUrl(this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/scayt.css\")");
-		FileUtils.writeStringToFile(file, fileContent);
-		
-		// modify wsc plugin to load wsc.css via CKEditor.getUrl
-		file = new File(resourcesDirectory
-                        + "ckeditor/ckeditor.js");
-		fileContent = FileUtils.readFileToString(file).replaceAll(
-                        "this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/wsc.css\"", 
-                        "CKEDITOR.getUrl(this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/wsc.css\")");
-		FileUtils.writeStringToFile(file, fileContent);
-	}
+            String fileContent = "";
+            File file = null;
 
-	private static List<File> getResourcesList(File file) {
-		List<File> result = new ArrayList<File>();
-		if (file.isDirectory()) {
-			for (File f : file.listFiles()) {
-				result.addAll(getResourcesList(f));
-			}
-		} else {
-			if (file.getName().endsWith(".png") || file.getName().endsWith(".css"))
-				result.add(file);
-		}
-		return result;
-	}
+            // modify smileys plugin to load the smileys via CKEditor.getUrl
+            file = new File(PROJECT_DIRECTORY
+                        + "/src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/plugins/smiley/dialogs/smiley.js");
+            fileContent = FileUtils.readFileToString(file).replaceAll(
+                        "CKEDITOR.tools.htmlEncode\\(e\\.smiley_path\\+h\\[a\\]\\)",
+                        "CKEDITOR.tools.htmlEncode\\(CKEDITOR.getUrl\\(e\\.smiley_path\\+h\\[a\\]\\)\\)");
+            FileUtils.writeStringToFile(file, fileContent);
+        }
+
+        // read the main JS file
+        final File file = new File(resourcesDirectory + "ckeditor/ckeditor.js");
+        String fileContent = FileUtils.readFileToString(file);
+
+        // modify copyFormatting plugin to load copyformatting.css via CKEditor.getUrl
+        fileContent = fileContent.replaceAll(
+                    "this.path\\+\"styles/copyformatting.css\"",
+                    "CKEDITOR.getUrl(this.path\\+\"styles/copyformatting.css\")");
+
+        // modify scayt plugin to load scayt.css via CKEditor.getUrl
+        fileContent = fileContent.replaceAll(
+                    "this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/scayt.css\"",
+                    "CKEDITOR.getUrl(this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/scayt.css\")");
+
+        // modify wsc plugin to load wsc.css via CKEditor.getUrl
+        fileContent = fileContent.replaceAll(
+                    "this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/wsc.css\"",
+                    "CKEDITOR.getUrl(this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/wsc.css\")");
+
+        // write file back out
+        FileUtils.writeStringToFile(file, fileContent);
+    }
+
+    private static List<File> getResourcesList(final File file) {
+        final String name = file.getName();
+        final List<File> result = new ArrayList<File>();
+        if (file.isDirectory()) {
+            for (final File f : file.listFiles()) {
+                result.addAll(getResourcesList(f));
+            }
+        }
+        else {
+            if (name.endsWith(".png") || name.endsWith(".css")) {
+                result.add(file);
+            }
+        }
+        return result;
+    }
 }
