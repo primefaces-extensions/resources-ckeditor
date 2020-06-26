6ffed163941c6e9505023dd77a8e22321945519b
melloware <mellowaredev@gmail.com>
2020-06-26 13:04:58 -0400

    Fix #804: Remove Commons Lang

--- src/main/java/org/primefaces/extensions/ckeditor/ResourceModifier.java
+++ src/main/java/org/primefaces/extensions/ckeditor/ResourceModifier.java
@@ -1,12 +1,9 @@
 /**
  * Copyright 2011-2018 PrimeFaces Extensions
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  * http://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,13 +17,12 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * Steps to update:
- * 
  * <pre>
  * - Delete all files in the src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/ directory
  * - extract new ckeditor files
@@ -35,7 +31,7 @@ import org.apache.commons.lang3.StringUtils;
  * - Before executing ResourceModifier, modify the short hash (e.g t=7f3189e) to match a new version of CKEditor
  * - Execute ResourceModifier
  * - Try to run CKEditor and observe if any error occurs on both browser console and server console
- *    - The most common error is "Resources not found". If that's the case, please check if any new plugins's resources that 
+ *    - The most common error is "Resources not found". If that's the case, please check if any new plugins's resources that
  *      need to be modified then add new rules inside ResourceModifier if needed
  *    - Re-run ResourceModifier
  * </pre>
@@ -43,8 +39,10 @@ import org.apache.commons.lang3.StringUtils;
 public class ResourceModifier {
 
     private static final String PROJECT_DIRECTORY = System.getProperty("user.dir");
-    
+
     private static final String SHORT_HASH = "7f3189e";
+
+    private static final int INDEX_NOT_FOUND = -1;
 
     public static void main(final String[] args) throws IOException {
 
@@ -54,16 +52,16 @@ public class ResourceModifier {
 
         final File skinsPath = new File(PROJECT_DIRECTORY + "/src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/skins/");
 
-        for (final String skin : skinsPath.list()) {
+        for (final String skin : Objects.requireNonNull(skinsPath.list())) {
             System.err.println("#### Modify skin '" + skin + "'");
 
             final File skinDirectory = new File(resourcesDirectory + "ckeditor/skins/" + skin);
-            final String relativeSkinPath = StringUtils.replace(
-                        StringUtils.replace(skinDirectory.getPath(), resourcesDirectory, ""),
+            final String relativeSkinPath = replace(
+                        replace(skinDirectory.getPath(), resourcesDirectory, ""),
                         "\\",
                         "/");
 
-            for (final File resourceToModify : skinDirectory.listFiles()) {
+            for (final File resourceToModify : Objects.requireNonNull(skinDirectory.listFiles())) {
 
                 // modify css only
                 if (!resourceToModify.getName().endsWith(".css")) {
@@ -83,11 +81,11 @@ public class ResourceModifier {
                         continue;
                     }
 
-                    final String relativeResourcePath = StringUtils.replace(
-                                StringUtils.replace(resource.getPath(), resourcesDirectory, ""),
+                    final String relativeResourcePath = replace(
+                                replace(resource.getPath(), resourcesDirectory, ""),
                                 "\\",
                                 "/");
-                    String resourceName = StringUtils.replace(
+                    String resourceName = replace(
                                 resource.getAbsolutePath().replace(skinDirectory.getAbsolutePath(), ""),
                                 "\\",
                                 "/");
@@ -102,7 +100,7 @@ public class ResourceModifier {
 
                 // icons.png
                 fileContent = fileContent.replaceAll("url\\(icons.png\\?t=" + SHORT_HASH,
-                            "url\\(\"#{resource['primefaces-extensions:" + relativeSkinPath + "/icons.png']}&t=" + SHORT_HASH +"\"");
+                            "url\\(\"#{resource['primefaces-extensions:" + relativeSkinPath + "/icons.png']}&t=" + SHORT_HASH + "\"");
 
                 // icons_hidpi.png
                 fileContent = fileContent.replaceAll("url\\(icons_hidpi.png\\?t=" + SHORT_HASH,
@@ -111,13 +109,10 @@ public class ResourceModifier {
                 FileUtils.writeStringToFile(resourceToModify, fileContent, Charset.defaultCharset());
             }
 
-            String fileContent = "";
-            File file = null;
-
             // modify smileys plugin to load the smileys via CKEditor.getUrl
-            file = new File(PROJECT_DIRECTORY
+            final File file = new File(PROJECT_DIRECTORY
                         + "/src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/plugins/smiley/dialogs/smiley.js");
-            fileContent = FileUtils.readFileToString(file, Charset.defaultCharset()).replaceAll(
+            final String fileContent = FileUtils.readFileToString(file, Charset.defaultCharset()).replaceAll(
                         "CKEDITOR.tools.htmlEncode\\(e\\.smiley_path\\+h\\[a\\]\\)",
                         "CKEDITOR.tools.htmlEncode\\(CKEDITOR.getUrl\\(e\\.smiley_path\\+h\\[a\\]\\)\\)");
             FileUtils.writeStringToFile(file, fileContent, Charset.defaultCharset());
@@ -141,14 +136,14 @@ public class ResourceModifier {
         fileContent = fileContent.replaceAll(
                     "this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/wsc.css\"",
                     "CKEDITOR.getUrl(this.path\\+\"skins/\"\\+CKEDITOR.skin.name\\+\"/wsc.css\")");
-        
+
         fileContent = fileContent.replaceAll(
-                "this.path\\+\"dialogs/dialog.css\"",
-                "CKEDITOR.getUrl(this.path\\+\"dialogs/dialog.css\")");
-        
+                    "this.path\\+\"dialogs/dialog.css\"",
+                    "CKEDITOR.getUrl(this.path\\+\"dialogs/dialog.css\")");
+
         fileContent = fileContent.replaceAll(
-                "this.path\\+\"styles/tableselection.css\"",
-                "CKEDITOR.getUrl(this.path\\+\"styles/tableselection.css\")");
+                    "this.path\\+\"styles/tableselection.css\"",
+                    "CKEDITOR.getUrl(this.path\\+\"styles/tableselection.css\")");
 
         // write file back out
         FileUtils.writeStringToFile(file, fileContent, Charset.defaultCharset());
@@ -156,9 +151,9 @@ public class ResourceModifier {
 
     private static List<File> getResourcesList(final File file) {
         final String name = file.getName();
-        final List<File> result = new ArrayList<File>();
+        final List<File> result = new ArrayList<>();
         if (file.isDirectory()) {
-            for (final File f : file.listFiles()) {
+            for (final File f : Objects.requireNonNull(file.listFiles())) {
                 result.addAll(getResourcesList(f));
             }
         }
@@ -168,5 +163,43 @@ public class ResourceModifier {
             }
         }
         return result;
+    }
+
+    public static int indexOf(final CharSequence seq, final CharSequence searchSeq, final int startPos) {
+        if (seq == null || searchSeq == null) {
+            return INDEX_NOT_FOUND;
+        }
+        return seq.toString().indexOf(searchSeq.toString(), startPos);
+    }
+
+    public static String replace(final String text, final String searchString, final String replacement) {
+        if (isEmpty(text) || isEmpty(searchString) || replacement == null) {
+            return text;
+        }
+        int max = INDEX_NOT_FOUND;
+        int start = 0;
+        int end = indexOf(text, searchString, start);
+        if (end == INDEX_NOT_FOUND) {
+            return text;
+        }
+        final int replLength = searchString.length();
+        int increase = replacement.length() - replLength;
+        increase = Math.max(increase, 0);
+        increase *= 16;
+        final StringBuilder buf = new StringBuilder(text.length() + increase);
+        while (end != INDEX_NOT_FOUND) {
+            buf.append(text, start, end).append(replacement);
+            start = end + replLength;
+            if (--max == 0) {
+                break;
+            }
+            end = indexOf(text, searchString, start);
+        }
+        buf.append(text, start, text.length());
+        return buf.toString();
+    }
+
+    public static boolean isEmpty(final CharSequence cs) {
+        return cs == null || cs.length() == 0;
     }
 }
